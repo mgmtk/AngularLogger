01e811eaf841e7ef36f2bde9d22ade9d1d6289cf
MGM <mesechmg@dukes.jmu.edu>
2020-05-15 00:38:09 -0400

    Worked on charting data

--- src/app/data/chartdata.ts
+++ src/app/data/chartdata.ts
@@ -15,36 +15,43 @@ export var chartOptions = {
     deferred: false
 }};
 
+/*
 export var  allTimeChartData = [ { 
     chartData: [{
         data: [200, 301, 242, 312, 280, 345],
         label: 'All Time Poops',
     }],
     labels:  ['2015', '2016', '2017', '2018', '2019', '2020']
+}]; */
+
+export var allTimeChartData = [ { 
+    data: [200, 301, 242, 312, 280, 345], label: 'All Time Poops'
 }];
+export var allTimeLabels = ['2015', '2016', '2017', '2018', '2019', '2020']
+
 
 export var  yearChartData = [{ 
         data: [40,32,67,23,31,55,62,21,13,45,68,11],
-        label: 'Yearly Poops',
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec']
-
+        label: 'Yearly Poops'
     }];
+export var yearLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'];
 
 export var monthChartData = [{ 
         data: [10,15,11,13],
-        label: 'Monthly Poops',
-        labels: ['Week1', 'Week2', 'Week3', 'Week4']
+        label: 'Monthly Poops'
     }];
+export var monthLabels = ['Week1', 'Week2', 'Week3', 'Week4'];
 
 export var weekChartData =  [{
         data: [1,2,3,3,2,4,1], 
-        label: 'Weekly Poops',
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
+        label: 'Weekly Poops'
+       
     }];
+export var weekLabels =  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 
 export var dayChartData: [{
      data: [7,6,5,4,3,2,1], 
-     label: 'Daily Poops' ,
-     labels:  ['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']
+     label: 'Daily Poops' 
     }];
-
+export var dayLabels = ['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']
--- src/app/tab3/chart/chart.component.ts
+++ src/app/tab3/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { chartOptions, allTimeChartData, yearChartData, monthChartData, weekChartData, dayChartData } from '../../data/chartdata';
+import { chartOptions, allTimeChartData, allTimeLabels,  yearChartData, monthChartData, weekChartData, dayChartData,
+yearLabels, monthLabels, weekLabels, dayLabels } from '../../data/chartdata';
 
 @Component({
     selector: 'chart-component',
@@ -18,25 +19,37 @@ import { chartOptions, allTimeChartData, yearChartData, monthChartData, weekChar
     weekChartData: any
     dayChartData: any
     chartOptions: any
+    allTimeLabels: any
+    yearLabels: any
+    monthLabels: any
+    weekLabels: any
+    dayLabels: any
    
     constructor() {
         Object.assign(this, { chartOptions });
         Object.assign(this, { allTimeChartData });
+        Object.assign(this, { allTimeLabels});
         Object.assign(this, { yearChartData});
+        Object.assign(this, { yearLabels});
         Object.assign(this, { monthChartData });
+        Object.assign(this, { monthLabels});
         Object.assign(this, { weekChartData });
+        Object.assign(this, { weekLabels});
         Object.assign(this, { dayChartData });
-        Object.assign(this, { chartOptions });
+        Object.assign(this, { dayLabels });
+
     }
 
     ngOnInit(){
-        this.chartData = this.dayChartData.data;
-        this.chartLabels = this.dayChartData.label
+        console.log(this.allTimeChartData)
+        this.chartData = this.allTimeChartData
+        this.chartLabels = this.allTimeLabels
+        
     }
 
-    changeData(changeData: any){
-        this.chartData = changeData.data
-        this.chartLabels = changeData.labels
+    changeData(changeData: any, changeLabels: any){
+        this.chartData = changeData
+        this.chartLabels = changeLabels
     }
 
 
