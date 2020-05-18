5e66d364ad1dbdbf3781f07202d749229d28d5c5
MGM <mesechmg@dukes.jmu.edu>
2020-05-18 09:35:54 -0400

    Worked on history comp css and firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDb_SKdQ79sq2kV22Lc6fKIAH4J2XZu9bM",
+    authDomain: "angularlogger-76310.firebaseapp.com",
+    databaseURL: "https://angularlogger-76310.firebaseio.com",
+    projectId: "angularlogger-76310",
+    storageBucket: "angularlogger-76310.appspot.com",
+    messagingSenderId: "182458062420",
+    appId: "1:182458062420:web:07a8ad056dd30354c97fc1",
+    measurementId: "G-F7GW719RB6"
+  }
 };
 
 /*
